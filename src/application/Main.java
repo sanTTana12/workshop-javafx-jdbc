71f3255dc1280f74c8d4ecb7b3dce47b6a4b50c7
Xaolin_do_38Malado <guilhermewandson737@gmail.com>
2026-03-18 14:09:23 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -11,6 +11,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -20,7 +22,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -28,6 +30,10 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
